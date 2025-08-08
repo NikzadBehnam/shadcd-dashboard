798ca4e8b43eaccbfcaa73b3d9ad8bef7f0ab8b8
NikzadBehnam <nikzad.behnam1992@gmail.com>
2025-08-08 11:36:11 +0200

    feat: implement initial layout for HomePage with grid structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { ImagePlus, Plus } from "lucide-react";
-
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test-1
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test-2</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test-3</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test-4</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test-5
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test-6</div>
+    </div>
+  );
 };
 
 export default HomePage;
